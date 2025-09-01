48209d8519e1c76b8a00e20e43d5f233f64decd7
jlmcgraw <jlmcgraw@gmail.com>
2025-09-01 11:36:27 -0400

    Minor updates
    More stringent mypy settings
    Changes to make lints and mypy happy

--- template/{{project_name}}/src/{{module_name}}/version.py
+++ template/{{project_name}}/src/{{module_name}}/version.py
@@ -1,17 +1,21 @@
-import tomllib
+"""docstring TBD."""
 from importlib import metadata
+import tomllib
 
 
 def get_version_from_metadata() -> str:
+    """Return the version from metadata."""
     return metadata.version(__package__ or __name__)
 
 
 def get_version_from_pyproject() -> str:
+    """Return the version from pyproject.toml."""
     with open("pyproject.toml", "rb") as file:
-        return tomllib.load(file)["project"]["version"]
+        return str(tomllib.load(file)["project"]["version"])
 
 
 def get_version() -> str:
+    """Return the version from metadata or pyproject.toml."""
     try:
         return get_version_from_metadata()
     except metadata.PackageNotFoundError:
